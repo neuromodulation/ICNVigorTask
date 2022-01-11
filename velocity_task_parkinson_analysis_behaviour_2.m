1d16e9e760868371b3e059a4931b12892d3f414d
unknown <alessia.cavallo16@gmail.de>
2022-01-11 14:20:22 +0100

    Changed summary plots

--- velocity_task_parkinson_analysis_behaviour_2.m
+++ velocity_task_parkinson_analysis_behaviour_2.m
@@ -7,8 +7,8 @@ close all;
 filenames = dir(fullfile('..\..\Data\Parkinson\',"*.mat"));
 n_files = length(filenames);
 n_trials = 33;
-n_conds = 3;
-n_blocks_all = [2,3,3];
+n_conds = 2;
+n_blocks = 3;
 conds = ["Slow","Fast"];
 subplot_indxs = [1 2; 3 4];
 
@@ -24,49 +24,40 @@ for i_file=1:n_files
     
     % Change the block order accordingly such that slow always comes first
     if options.cond
-        blocks_cond = {1:2; 3:5; 9:11};
+        blocks_cond = [3:5; 9:11];
     else
-        blocks_cond = {1:2 ; 9:11; 3:5};
+        blocks_cond = [9:11; 3:5];
     end
     
     % Create a summary plot for each participant
     FigH = figure('Position', get(0, 'Screensize')); 
     sgtitle(filenames(i_file).name);
     perf = [];
+    three_after_stim = [];
+    diffs_stim_peak_all = [];
+    diffs_stim_peak_all_i = [];
     %% Loop over every movement
     peaks_all = [];
     for i_cond = 1:n_conds
         peaks = [];
         stim = [];
         true_stim = [];
-        diffs_target_stim = [];
         diffs_stim_peak = [];
-        n_blocks = n_blocks_all(i_cond);
-        block_number = cell2mat(blocks_cond(i_cond));
         for i_block=1:n_blocks
             for i_trial=2:n_trials
                 
                 % Get the data from one trial
-                mask = data(:,8) == block_number(i_block) & data(:,9) == i_trial;
+                mask = data(:,8) == blocks_cond(i_cond,i_block) & data(:,9) == i_trial;
                 data_trial = data(mask, :);
-                
-                % Get the mean velocity over 4 instead of 7 samples
-                data_vel_av = zeros(length(data_trial),1);
-                for i=4:length(data_trial)
-                    data_vel_av(i) = mean(data_trial(i-3:i,5));
-                end
-                
-                % Find the index of the target
-                ind_target = find(data_trial(:,10)==1,1);
             
                 % Find the peak 
-                [peak,ind_peak] = max(data_vel_av);
+                [peak,ind_peak] = max(data_trial(:,4));
                 peaks = cat(1,peaks, peak);
                 
                 % Check if the trial should be simulated (based on true
                 % peak)
-                if length(peaks)> 2 && (all(peaks(end-2:end-1) > peaks(end)) && i_cond-1 == 1 ||...
-                    all(peaks(end-2:end-1)< peaks(end)) && i_cond-1 == 2)
+                if length(peaks)> 2 && (all(peaks(end-2:end-1) > peaks(end)) && i_cond == 1 ||...
+                    all(peaks(end-2:end-1)< peaks(end)) && i_cond == 2)
                     true_stim = cat(1,true_stim, 1);
                 else
                     true_stim = cat(1,true_stim, 0);
@@ -79,85 +70,79 @@ for i_file=1:n_files
                 % peak/stimulation and stimulation/target
                 if ind_stim 
                     stim = cat(1,stim, 1);
-                    diffs_stim_peak = cat(1,diffs_stim_peak, data_trial(ind_stim,3)-data_trial(ind_peak,3));
-                    diffs_target_stim = cat(1,diffs_target_stim, data_trial(ind_target,3)-data_trial(ind_stim,3));
-                else
+                    diffs_stim_peak = cat(1,diffs_stim_peak, data_trial(ind_stim,3)-data_trial(ind_peak,3));                else
                     stim = cat(1,stim, 0);
                 end
             end
         end
-        % For the first two blocks (without stimulation) get the difference
-        % between sonsecutive peaks after slow/fast peaks
-        if i_cond == 1
-            % Get the index of slow and fast peaks 
-            slow_ind = []; fast_ind = [];
-            for i=3:length(peaks)-1
-                if all(peaks(i-2:i-1) > peaks(i)) 
-                    slow_ind = cat(1,slow_ind,i);
-                elseif all(peaks(i-2:i-1) < peaks(i))
-                    fast_ind = cat(1,fast_ind,i);
-                end
-            end
-            % Get the difference in peak velocity between consecutive
-            % trials
-            diff_slow = peaks(slow_ind) - peaks(slow_ind+1);
-            diff_fast = peaks(fast_ind) - peaks(fast_ind+1);
-            diffs_baseline = {diff_slow, diff_fast};
-        else
-            % After all blocks from one condition are processed 
-            % Replace outliers with the linear method
-            peaks = filloutliers(peaks,"linear");
-            diffs_stim_peak = filloutliers(diffs_stim_peak,"linear");
-            diffs_target_stim = filloutliers(diffs_target_stim, "linear");
+        % After all blocks from one condition are processed 
+        % Replace outliers with the linear method
+        peaks = filloutliers(peaks,"linear");
+        diffs_stim_peak = filloutliers(diffs_stim_peak,"linear");
 
-            % Get the indexes of the movements after stimulation
-            ind_after_stim = find(stim == 1) + 1; 
-            % If the last movement was stimulated delete the entry
-            if any(ind_after_stim > length(peaks))
-                ind_after_stim = ind_after_stim(1:end-1);
-                diffs_stim_peak = diffs_stim_peak(1:end-1);
-                diffs_target_stim= diffs_target_stim(1:end-1);
-            end
-
-            % Plot the scatter plot of the velocity of the movement after
-            % stimulation and the time between the peak and stimulation
-            subplot(2,3,subplot_indxs(i_cond-1, 1));
-            scatter(peaks(ind_after_stim),diffs_stim_peak);
-            r = corrcoef(peaks(ind_after_stim),diffs_stim_peak);
-            title(conds(i_cond-1) + " " + string(r(1,2)));
-            xlabel("Peak velocity of next trial");
-            ylabel("Time from peak to stimulation (sec)");
-
-            % Plot the difference in peak velocity for movements after a 
-            % a slow/fast movement for stimulated and not stimulated
-            % movements
-            subplot(2,3,subplot_indxs(i_cond-1,2));
-            diff_peak_after_stim = peaks(ind_after_stim-1) - peaks(ind_after_stim);
-            diff_peak_no_stim = cell2mat(diffs_baseline(i_cond-1));
-            boxplot([diff_peak_after_stim; diff_peak_no_stim],...
-                    [ones(length(diff_peak_after_stim),1); ones(length(diff_peak_no_stim),1)*2],...
-                    'Labels',["stim","no stim"]);
-            title(conds(i_cond-1));
-            ylabel("Difference of peak velocity to next trial");
-
-            % Save the peaks normalized to the first 15 movements 
-            peaks = peaks - mean(peaks(1:15)); 
-            % Average over 5 consecutive movements
-            peaks_mean = mean(reshape(peaks(1:end-mod(length(peaks),5)),5,[]),1);
-            x = linspace(1,length(peaks),length(peaks_mean));
-            peaks_all = cat(1, peaks_all, peaks_mean);
-
-            % Calculate the number of stimulated movements as well as sensitivity
-            % and specificity
-            perc_stim = sum(stim)/length(stim);
-            sens = sum(stim == 1 & true_stim == 1)/sum(true_stim == 1);
-            spec = sum(stim == 0 & true_stim == 0)/sum(true_stim == 0);
-            perf = cat(1,perf,[perc_stim, sens,spec]*100);
+        % Get the indexes of the movements after stimulation
+        ind_after_stim = find(stim == 1) + 1; 
+        % If the last movement was stimulated delete the entry
+        if any(ind_after_stim > length(peaks))
+            ind_after_stim = ind_after_stim(1:end-1);
+            diffs_stim_peak = diffs_stim_peak(1:end-1);
         end
+
+        % Plot the scatter plot of the velocity of the movement after
+        % stimulation and the time between the peak and stimulation
+        subplot(2,3,i_cond);
+        scatter(peaks(ind_after_stim),abs(diffs_stim_peak));
+        r = corrcoef(peaks(ind_after_stim),abs(diffs_stim_peak));
+        title(conds(i_cond) + " " + string(r(1,2)));
+        xlabel("Peak velocity of next trial");
+        ylabel("Time from peak to stimulation (sec)");
+        % Save the time difference between stim and peak 
+        diffs_stim_peak_all = cat(1, diffs_stim_peak_all,diffs_stim_peak);
+        diffs_stim_peak_all_i = cat(1, diffs_stim_peak_all_i,ones(length(diffs_stim_peak),1)*i_cond);
+
+        % Save the peaks normalized to the first 15 movements 
+        peaks = peaks - mean(peaks(1:15)); 
+        % Average over 5 consecutive movements
+        peaks_mean = mean(reshape(peaks(1:end-mod(length(peaks),5)),5,[]),1);
+        x = linspace(1,length(peaks),length(peaks_mean));
+        peaks_all = cat(1, peaks_all, peaks_mean);
+
+        % Calculate the number of stimulated movements as well as sensitivity
+        % and specificity
+        perc_stim = sum(stim)/length(stim);
+        sens = sum(stim == 1 & true_stim == 1)/sum(true_stim == 1);
+        spec = sum(stim == 0 & true_stim == 0)/sum(true_stim == 0);
+        perf = cat(1,perf,[perc_stim, sens,spec]*100);
+        
+        % Get the index of stimulated movements after which at least 3
+        % trials follow
+        ind_stim = find(stim == 1); 
+        ind_stim = ind_stim(ind_stim <= length(stim) - 3);
+        % Get always the three movements after a stimulation 
+        peaks_after_stim = [];
+        for i=1:length(ind_stim)
+            peaks_after_stim = cat(2,peaks_after_stim, peaks(ind_stim(i)+1:ind_stim(i)+3));
+        end
+        % Average over all stimulations and substract the peak of the
+        % stimulation
+        mean_peaks_after_stim = mean(peaks_after_stim - peaks(ind_stim).', 2);
+        % Save it 
+        three_after_stim = cat(2, three_after_stim, mean_peaks_after_stim);
     end
+    subplot(2,3,3);
+    % Plot the difference of peak velocity of the three trials after
+    % stimulation to the stimulated trial
+    for i=1:2
+        scatter([1,2,3], three_after_stim(:,i));
+        plot(three_after_stim(:,i));
+        hold on;
+    end
+    legend(["Slow" "Fast"]);
+    title("Av. difference of peak velocity to stim trial");
+    xlabel("Trial number after stim");
     
     % Plot the normalized change in velocity 
-    subplot(2,3,5);
+    subplot(2,3,4);
     plot(x,peaks_all(1,:)); hold on; 
     plot(x,peaks_all(2,:)); 
     legend(conds);
@@ -165,7 +150,7 @@ for i_file=1:n_files
     ylabel("Normalized peak velocity");
     
     % Plot the performance of the task (Stimulation of correct movements)
-    subplot(2,3,6);
+    subplot(2,3,5);
     perf = round(perf,0);
     b = bar(perf);
     ylabel("Percentage");
@@ -181,6 +166,14 @@ for i_file=1:n_files
         x = linspace(i-offset, i+offset, length(row));
         text(x,max(row-40,10),num2str(row'),'vert','bottom','horiz','center');
     end
+    
+    % Plot the time difference between stim and peak for both conditions
+    % in a box plot 
+    subplot(2,3,6);
+    boxplot(diffs_stim_peak_all,diffs_stim_peak_all_i);
+    set(gca, 'XTickLabel', {"Slow" "Fast"});
+    ylabel("Time from peak to stim (sec)");
+    
     % Save the figure 
     saveas(FigH, sprintf('../../Plots/Dataset%i.png', i_file), 'png');
 
