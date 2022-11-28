7744f9785611476b1dbc81833606c97fc0bc8128
unknown <alessia.cavallo16@gmail.de>
2022-11-28 12:29:50 +0100

    Synchronized

---
+++ analysis/analysis_new.py
@@ -0,0 +1,81 @@
+# Script for further analysis
+# Cumlative change in velocity
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+import easygui
+import os
+from ICNVigorTask.utils.utils import reshape_data_trials, norm_speed, smooth_moving_average, plot_speed, fill_outliers, norm_perf_speed
+
+# Set analysis parameters
+plot_individual = True
+med = "MedOn"  #"MedOff"
+
+# Get list of all datasets
+path = "D:\\raw_data\\"
+folder_list = os.listdir(path)
+files_list = []
+# Loop over every subject
+for subject_folder in folder_list:
+    # Get the brainvision files for that subject
+    for root, dirs, files in os.walk(path+subject_folder):
+        for file in files:
+            if (file.endswith(".vhdr")) and "VigorStim" in file and med in file:
+                files_list.append(os.path.join(root, file))
+
+# Plot the speed of all datasets
+peak_speed_all = []
+peak_speed_cum_all = []
+for file in files_list:
+
+    # Load the dataset of interest
+    raw_data = mne.io.read_raw_brainvision(file, preload=True)
+
+    # Get the channel index of the mean speed values
+    mean_speed_idx = raw_data.info["ch_names"].index("mean_vel")
+
+    # Structure data in trials and blocks
+    data = reshape_data_trials(raw_data)
+
+    # Extract the peak speed of all trials
+    peak_speed = np.max(data[:, :, :, mean_speed_idx, :], axis=3)
+
+    # Detect and fill outliers (e.g. when subject did not touch the screen)
+    x = np.apply_along_axis(lambda m: fill_outliers(m), axis=2, arr=peak_speed)
+
+    # Normalize them to the start speed
+    peak_speed = norm_speed(peak_speed)
+
+    # Compute the cumulative change in peak speed
+    peak_speed_cum = np.cumsum(peak_speed, axis=2)
+    peak_speed_cum[:,1,:] += peak_speed_cum[:,0,-1][:, np.newaxis]
+
+    # Plot if needed
+    if plot_individual:
+        plt.figure()
+        plt.subplot(1,2,1)
+        plot_speed(peak_speed)
+        plt.subplot(1,2,2)
+        plot_speed(peak_speed_cum)
+        plt.legend()
+        plt.suptitle(file.split("\\")[-1])
+        #plt.show()
+
+    # Save the speed values for all datasest
+    peak_speed_all.append(peak_speed)
+    peak_speed_cum_all.append(peak_speed_cum)
+
+# Average over all datasets
+peak_speed_all = np.array(peak_speed_all)
+mean_peak_speed = np.mean(peak_speed_all, axis=0)
+peak_speed_cum_all = np.array(peak_speed_cum_all)
+mean_peak_speed_cum = np.mean(peak_speed_cum_all, axis=0)
+# Plot
+plt.figure()
+plt.subplot(1,2,1)
+plot_speed(mean_peak_speed)
+plt.subplot(1,2,2)
+plot_speed(mean_peak_speed_cum)
+plt.title("Average")
+plt.show()
--- preprocessing/vigor_stim_task_add_behavioral_data.py
+++ preprocessing/vigor_stim_task_add_behavioral_data.py
@@ -1,123 +1,143 @@
+# Add synchronized behavioral data to brain vision file
+
 import matplotlib.pyplot as plt
 import mne
 from scipy.io import loadmat, savemat
 from scipy.stats import zscore
 import numpy as np
 import pandas as pd
-import easygui
+import os
 from ICNVigorTask.utils.utils import norm_0_1
 
-# Add synchronized behavioral data to brain vision file
+# Get list of all brainvision and matlab datasets
+path = "D:\\raw_data\\"
+folder_list = os.listdir(path)
+files_raw_list = []
+files_mat_list = []
+# Loop over every subject
+for subject_folder in folder_list:
+    # Get the brainvision files for that subject
+    for root, dirs, files in os.walk(path+subject_folder):
+        for file in files:
+            if (file.endswith(".vhdr")) and "VigorStim" in file:
+                files_raw_list.append(os.path.join(root, file))
+                # Add matlab file
+                for file in os.listdir(root):
+                    if file.endswith(".mat"):
+                        files_mat_list.append(os.path.join(root, file))
 
-# Load the TMSi data
-#filename_neuro = easygui.fileopenbox(default="*.vhdr")
-filename_neuro = "C:\\Users\\alessia\\Documents\\Jobs\\ICN\\vigor-stim\\Data\\Archive\\archive_ICN_data\\Parkinson_Pilot\\Neuro data\\sub-06-MedOff-task-VigorStim-R-Fast-Slow-StimOn-run-01-neuro\\sub-008_ses-EphysMedOff01_task-VigorStimR_acq-StimOn_run-01_ieeg.vhdr"
-raw_data = mne.io.read_raw_brainvision(filename_neuro, preload=True)
 
-# Load the MATLAB data
-#filename_behav = easygui.fileopenbox(default="*.mat")
-filename_behav = "C:\\Users\\alessia\\Documents\\Jobs\\ICN\\vigor-stim\\Data\\Archive\\archive_ICN_data\\Parkinson_Pilot\\sub-06-MedOff-task-VigorStim-R-Fast-Slow-StimOn-run-01_behavioral.mat"
-behav_data = loadmat(filename_behav)
-# Extract the behavioral data stored in a matrix
-behav_data = behav_data["struct"][0][0][1]
-# Determine the condition based on the filename
-slow_first = 1 if filename_behav.index("Slow") < filename_behav.index("Fast") else 0
+# Loop through all Vigor Stim datasets and add matlab data to brainvision file
+for filename_raw, filename_mat in zip(files_raw_list, files_mat_list):
 
-# Downsample the neuro data to 500 Hz
-raw_data.resample(500)
+    # Load the TMSi data
+    raw_data = mne.io.read_raw_brainvision(filename_raw preload=True)
 
-# Get the times of the samples
-time_array_neuro = raw_data.times.flatten()
+    # Load the MATLAB data
 
-# Determine stimulation onset base don LFP channels
+    behav_data = loadmat(filename_mat)
+    # Extract the behavioral data stored in a matrix
+    behav_data = behav_data["struct"][0][0][1]
+    # Determine the condition based on the filename
+    slow_first = 1 if filename_mat.index("Slow") < filename_mat.index("Fast") else 0
 
-# Filter the data
-raw_data_filt = raw_data.copy().filter(l_freq=2, h_freq=200)
-# Average first 10 channels (cut out the last 100 samples because of end artifact)
-data_mean = np.mean(raw_data_filt._data[:10,:-1000], axis=0)
+    # Downsample the neuro data to 500 Hz
+    raw_data.resample(500)
 
-# Plot for visual inspection
-plt.figure()
-plt.plot(data_mean)
+    # Get the times of the samples
+    time_array_neuro = raw_data.times.flatten()
 
-# Find the first sample above a threshold
-idx_onset_neuro = np.where(np.abs(zscore(data_mean)) > 2.5)[0][0]
+    # Determine stimulation onset base don LFP channels
 
-# Plot for visual inspection
-plt.axvline(idx_onset_neuro, color="red")
-#plt.show()
+    # Filter the data
+    raw_data_filt = raw_data.copy().filter(l_freq=2, h_freq=200)
+    # Average first 10 channels (cut out the last 100 samples because of end artifact)
+    data_mean = np.mean(raw_data_filt._data[:10,:-1000], axis=0)
 
-# Find the first sample with stimulation in the behavioral data
-behav_data_stim = behav_data[:, 10].flatten()
-idx_onset_behav = np.where(behav_data_stim == 1)[0][0]
+    # Plot for visual inspection
+    plt.figure()
+    plt.plot(data_mean)
 
-# Get time in sec at which stimulation onsets occur
-time_onset_neuro = time_array_neuro[idx_onset_neuro]
-time_array_behav = behav_data[:, 2].flatten()
-time_onset_behav = time_array_behav[idx_onset_behav]
+    # Find the first sample above a threshold
+    idx_onset_neuro = np.where(np.abs(zscore(data_mean)) > 2.5)[0][0]
 
-# Substract the time difference from the neuro data (neuro recording alsways starts first)
-diff_time = time_onset_neuro - time_onset_behav
-time_array_neuro = time_array_neuro - diff_time
+    # Plot for visual inspection
+    plt.axvline(idx_onset_neuro, color="red")
+    #plt.show()
 
-# Get indexes of stimulation onset in behav data and visually check the alignment
-plt.figure()
-idx_stim = np.where(np.diff(behav_data_stim) == 1)[0]
-plt.plot(time_array_neuro[:len(data_mean)], data_mean)
-for idx in idx_stim:
-    plt.axvline(time_array_behav[idx], color="red")
-#plt.show()
+    # Find the first sample with stimulation in the behavioral data
+    behav_data_stim = behav_data[:, 10].flatten()
+    idx_onset_behav = np.where(behav_data_stim == 1)[0][0]
 
-# For every sample in the neuro data find the closest sample in the behav data
-n_cols = np.size(behav_data, 1)
-behav_data_long = np.zeros((len(time_array_neuro), n_cols))
-for i, time_samp in enumerate(time_array_neuro):
-    # If neuro sample is before or after onset of behav recording, save zeros
-    if time_samp < 0 or time_samp > np.max(time_array_behav):
-        behav_data_long[i, :] = np.zeros(16)
-    else:
-        # Get the sample that is closest in time
-        idx_samp_behav = np.argmin(np.abs(time_array_behav - time_samp))
-        behav_data_long[i,:] = behav_data[idx_samp_behav,:]
+    # Get time in sec at which stimulation onsets occur
+    time_onset_neuro = time_array_neuro[idx_onset_neuro]
+    time_array_behav = behav_data[:, 2].flatten()
+    time_onset_behav = time_array_behav[idx_onset_behav]
 
-# Add a channel containing the condition
-cond = [0 if i in [1, 2] and slow_first or i in [3, 4] and not slow_first else 1 for i in behav_data_long[:, 7]]
-behav_data_long = np.hstack((behav_data_long, np.array(cond)[:, np.newaxis]))
+    # Substract the time difference from the neuro data (neuro recording alsways starts first)
+    diff_time = time_onset_neuro - time_onset_behav
+    time_array_neuro = time_array_neuro - diff_time
 
-# Select channels that should be saved
-behav_data_long = behav_data_long[:, [0,1,3,4,7,8,9,10,11,12,16]]
+    # Get indexes of stimulation onset in behav data and visually check the alignment
+    plt.figure()
+    idx_stim = np.where(np.diff(behav_data_stim) == 1)[0]
+    plt.plot(time_array_neuro[:len(data_mean)], data_mean)
+    for idx in idx_stim:
+        plt.axvline(time_array_behav[idx], color="red")
+    #plt.show()
 
-# Add behavioral channels to the raw mne object
-ch_names = ["PEN_X", "PEN_Y", "SPEED_MEAN", "SPEED", "BLOCK", "TRIAL", "TARGET", "STIMULATION", "TARGET_X", "TARGET_Y", "STIM_CONDITION"]
-info = mne.create_info(ch_names, raw_data.info['sfreq'], ["bio"]*len(ch_names))
-behav_raw = mne.io.RawArray(behav_data_long.T, info)
-raw_data.add_channels([behav_raw], force_update_info=True)
+    # For every sample in the neuro data find the closest sample in the behav data
+    n_cols = np.size(behav_data, 1)
+    behav_data_long = np.zeros((len(time_array_neuro), n_cols))
+    for i, time_samp in enumerate(time_array_neuro):
+        # If neuro sample is before or after onset of behav recording, save zeros
+        if time_samp < 0 or time_samp > np.max(time_array_behav):
+            behav_data_long[i, :] = np.zeros(16)
+        else:
+            # Get the sample that is closest in time
+            idx_samp_behav = np.argmin(np.abs(time_array_behav - time_samp))
+            behav_data_long[i,:] = behav_data[idx_samp_behav,:]
 
-# Final plot for visual inspection
-plt.figure()
-plt.plot(raw_data.get_data(["STIMULATION"]).T)
-plt.plot(norm_0_1(data_mean))
-plt.show()
+    # Add a channel containing the condition
+    cond = [0 if i in [1, 2] and slow_first or i in [3, 4] and not slow_first else 1 for i in behav_data_long[:, 7]]
+    behav_data_long = np.hstack((behav_data_long, np.array(cond)[:, np.newaxis]))
 
-# Save new brain vision file
-filename_new = filename_neuro[:-5] + "_new.vhdr"
-mne.export.export_raw(fname=filename_neuro, raw=raw_data, fmt="brainvision", overwrite=True)
+    # Select channels that should be saved
+    behav_data_long = behav_data_long[:, [0,1,3,4,7,8,9,10,11,12,16]]
 
-# Add channels to corresponding tsv file
-tsv_filename = filename_neuro[:-4]+"tsv"
-tsv_file = pd.read_csv(tsv_filename, sep='\t')
-template = tsv_file.loc[len(tsv_file)-1]
-template[-1] = "Task"
-template[-2] = "Task"
-for ch_name in ch_names:
-    new_row = template.copy()
-    new_row[0] = ch_name
-    tsv_file = tsv_file.append(new_row)
-# Update the sampling frequency
-tsv_file.sampling_frequency = np.ones(len(tsv_file)) * 500
-tsv_file.high_cutoff = np.ones(len(tsv_file)) * 250
-# Save updated tsv file
-with open(tsv_filename,'w') as write_tsv:
-    write_tsv.write(tsv_file.to_csv(sep='\t', index=False))
+    # Add behavioral channels to the raw mne object
+    ch_names = ["PEN_X", "PEN_Y", "SPEED_MEAN", "SPEED", "BLOCK", "TRIAL", "TARGET", "STIMULATION", "TARGET_X", "TARGET_Y", "STIM_CONDITION"]
+    info = mne.create_info(ch_names, raw_data.info['sfreq'], ["bio"]*len(ch_names))
+    behav_raw = mne.io.RawArray(behav_data_long.T, info)
+    raw_data.add_channels([behav_raw], force_update_info=True)
 
-print("Successfully added behavioral data")
+    # Final plot for visual inspection
+    plt.figure()
+    plt.plot(raw_data.get_data(["STIMULATION"]).T)
+    plt.plot(norm_0_1(data_mean))
+    plt.show()
+
+    # Save new brain vision file
+    filename_new = filename_raw[:-5] + "_new.vhdr"
+    mne.export.export_raw(fname=filename_new, raw=raw_data, fmt="brainvision", overwrite=True)
+
+    # Add channels to corresponding tsv file
+    tsv_filename = filename_raw[:-4]+"tsv"
+    tsv_file = pd.read_csv(tsv_filename, sep='\t')
+    template = tsv_file.loc[len(tsv_file)-1]
+    template[-1] = "Task"
+    template[-2] = "Task"
+    for ch_name in ch_names:
+        new_row = template.copy()
+        new_row[0] = ch_name
+        tsv_file = tsv_file.append(new_row)
+    # Update the sampling frequency
+    tsv_file.sampling_frequency = np.ones(len(tsv_file)) * 500
+    tsv_file.high_cutoff = np.ones(len(tsv_file)) * 250
+    # Save updated tsv file
+    with open(tsv_filename[:-4]+"_new.tsv",'w') as write_tsv:
+        write_tsv.write(tsv_file.to_csv(sep='\t', index=False))
+
+    print(f"Successfully added behavioral data to {filename_raw}")
+    
+    plt.close()
